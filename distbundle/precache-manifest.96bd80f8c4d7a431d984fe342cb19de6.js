2f885fb9141c41cb57b98a231face19a9969cb7a
Leo <yzy0806@gmail.com>
2021-04-23 18:45:10 +0800

    initial commit

---
+++ distbundle/precache-manifest.96bd80f8c4d7a431d984fe342cb19de6.js
@@ -0,0 +1,313 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ff76b1826e6408ef941d6e9e9f285",
+    "url": "./bundle../dist/index.html"
+  },
+  {
+    "url": "./bundleapp.54a0eecbb3dc7d75cf83.css"
+  },
+  {
+    "revision": "9922c0f69aeb8e9820a583f784453762",
+    "url": "./bundleimages/amex.svg"
+  },
+  {
+    "revision": "2edbf568491472c1f48ed97cd1abfacf",
+    "url": "./bundleimages/arrow-back.svg"
+  },
+  {
+    "revision": "24d174f4a5a1a34940852d713a66cec0",
+    "url": "./bundleimages/arrow.svg"
+  },
+  {
+    "revision": "16b67a69b11e4adb99eede9a5e8eff46",
+    "url": "./bundleimages/breadcrumbs-arrow.svg"
+  },
+  {
+    "revision": "46e5dfb2cdad1754ee16cd521e551b7a",
+    "url": "./bundleimages/carousel-arrow.svg"
+  },
+  {
+    "revision": "2e279873fa0479e27bf8ff74f99eedd8",
+    "url": "./bundleimages/cart.svg"
+  },
+  {
+    "revision": "423a626c9c5659e6f50e9fc0fb8fff18",
+    "url": "./bundleimages/discover.svg"
+  },
+  {
+    "revision": "f03631a22bd42b06d0501714fffe5178",
+    "url": "./bundleimages/facebook-icon.svg"
+  },
+  {
+    "revision": "b3361a65b1eddda83705dbeed96a283f",
+    "url": "./bundleimages/favicons/android-chrome-144x144.png"
+  },
+  {
+    "revision": "2cd2aff91db66d9327d1452bfb84106f",
+    "url": "./bundleimages/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "08383e70706c58ccff35d55382ad0005",
+    "url": "./bundleimages/favicons/android-chrome-256x256.png"
+  },
+  {
+    "revision": "e19ebe863ab546d13ee363d7a45ebb09",
+    "url": "./bundleimages/favicons/android-chrome-36x36.png"
+  },
+  {
+    "revision": "b266f8922396c26cb7ea7f703d9eca74",
+    "url": "./bundleimages/favicons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "3603e288390fae80a76e3eae28dcc0b5",
+    "url": "./bundleimages/favicons/android-chrome-48x48.png"
+  },
+  {
+    "revision": "c486409a698400185bc4f07c6e19f2bd",
+    "url": "./bundleimages/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "589ea1c1b26841504825c27d4f5fc19e",
+    "url": "./bundleimages/favicons/android-chrome-72x72.png"
+  },
+  {
+    "revision": "7239e1aa266c0cc68cbf6d14f8eb83fe",
+    "url": "./bundleimages/favicons/android-chrome-96x96.png"
+  },
+  {
+    "revision": "fd8a304cb071df95c585116890d752b7",
+    "url": "./bundleimages/favicons/apple-touch-icon-1024x1024.png"
+  },
+  {
+    "revision": "451411e49757b6c6361c62232fb5cfce",
+    "url": "./bundleimages/favicons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "03a6d7ccd3c33e887fc8a485682e78e1",
+    "url": "./bundleimages/favicons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "4870b951aea7696f64a02a441318d356",
+    "url": "./bundleimages/favicons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "01d0b0cd86aeaafcf36452ca3a2b773e",
+    "url": "./bundleimages/favicons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "946ac375e41368108db9cb8f87da9d9d",
+    "url": "./bundleimages/favicons/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "5c75f11430cae0b3626a3e993398c590",
+    "url": "./bundleimages/favicons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "f2b818d43ed50ab013cbdb9468c235d1",
+    "url": "./bundleimages/favicons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "e5b0a110124fa9ce98dc711aa583fbdb",
+    "url": "./bundleimages/favicons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "72b4c84c3c6b5fb6522817b819efe490",
+    "url": "./bundleimages/favicons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "ca7470316e66d6d3eb324f34f1b41ff0",
+    "url": "./bundleimages/favicons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "5c75f11430cae0b3626a3e993398c590",
+    "url": "./bundleimages/favicons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "5c75f11430cae0b3626a3e993398c590",
+    "url": "./bundleimages/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "9bd9c68ef2f1cc2099d98b63415fab26",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-1182x2208.png"
+  },
+  {
+    "revision": "7da4b5ce7bd388a0f9c2347b483a954a",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-1242x2148.png"
+  },
+  {
+    "revision": "40c92c8de1571de0c67234b0d543f0da",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-1496x2048.png"
+  },
+  {
+    "revision": "664fb1788a1743b5f7501fc9682d38ca",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-1536x2008.png"
+  },
+  {
+    "revision": "65e7b353321ebea4b719bc03e99cec84",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-320x460.png"
+  },
+  {
+    "revision": "5d77699dc552b62c3b69b6f2a4184657",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-640x1096.png"
+  },
+  {
+    "revision": "7e924e928cf2e5cc9f125164d78a7a36",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-640x920.png"
+  },
+  {
+    "revision": "ff2e76cf745910d36b5f9933f7c0e0b4",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-748x1024.png"
+  },
+  {
+    "revision": "ab392b4506a95960dcc7ed4afd623a52",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-750x1294.png"
+  },
+  {
+    "revision": "ac67e1324e8e75035916af39c54c7c50",
+    "url": "./bundleimages/favicons/apple-touch-startup-image-768x1004.png"
+  },
+  {
+    "revision": "c4be82d860edec5dd7c0c253b40dcb54",
+    "url": "./bundleimages/favicons/browserconfig.xml"
+  },
+  {
+    "revision": "e6ceabdae52d437ec8b49f6bad6e594a",
+    "url": "./bundleimages/favicons/coast-228x228.png"
+  },
+  {
+    "revision": "38481ccba1cb1c9f813041e7ecfb1ba2",
+    "url": "./bundleimages/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "677ad5feb0aedf8dd890bae9f22b21ac",
+    "url": "./bundleimages/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "e4f027b5da67307190f5d152dd0615e0",
+    "url": "./bundleimages/favicons/favicon.ico"
+  },
+  {
+    "revision": "1f651fc2ac103df532a6b393ba9e657e",
+    "url": "./bundleimages/favicons/firefox_app_128x128.png"
+  },
+  {
+    "revision": "e162a1ac34c8b47a2861982b89dcfe30",
+    "url": "./bundleimages/favicons/firefox_app_512x512.png"
+  },
+  {
+    "revision": "f1570047ba5f27ee35b65d7993cc1844",
+    "url": "./bundleimages/favicons/firefox_app_60x60.png"
+  },
+  {
+    "revision": "44c533c6fbb211a926452d2c069e8f32",
+    "url": "./bundleimages/favicons/manifest.json"
+  },
+  {
+    "revision": "ba08b7100158eb81ef34f977a7f1b2c6",
+    "url": "./bundleimages/favicons/manifest.webapp"
+  },
+  {
+    "revision": "b3361a65b1eddda83705dbeed96a283f",
+    "url": "./bundleimages/favicons/mstile-144x144.png"
+  },
+  {
+    "revision": "85d474e4d8fb29aee471d2351fe26730",
+    "url": "./bundleimages/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "0ef20bca7226c25db81476e334414f0d",
+    "url": "./bundleimages/favicons/mstile-310x150.png"
+  },
+  {
+    "revision": "a8673ca3a99910c66e2e00c2a7e04bb1",
+    "url": "./bundleimages/favicons/mstile-310x310.png"
+  },
+  {
+    "revision": "86c15d37374ca1e844feb2417db14d48",
+    "url": "./bundleimages/favicons/mstile-70x70.png"
+  },
+  {
+    "revision": "7c2abcb9e4492e0bc6eaeeccb7d4df7c",
+    "url": "./bundleimages/favicons/yandex-browser-50x50.png"
+  },
+  {
+    "revision": "964aa63186fe3151346c28e8f44da2da",
+    "url": "./bundleimages/favicons/yandex-browser-manifest.json"
+  },
+  {
+    "revision": "72f0b80556965d101b99735a5382c1b5",
+    "url": "./bundleimages/garbage.svg"
+  },
+  {
+    "revision": "60ba99ba49ebcb48acf3455ee22ef22b",
+    "url": "./bundleimages/hamburger-hover.svg"
+  },
+  {
+    "revision": "5bacf6d70b4037a57f01cf8393e0b4d9",
+    "url": "./bundleimages/hamburger.svg"
+  },
+  {
+    "revision": "280995e507a9a208edcfad3d1bdda1cf",
+    "url": "./bundleimages/instagram-icon.svg"
+  },
+  {
+    "revision": "35555a736d713af1d68af3f399b116f8",
+    "url": "./bundleimages/jcb.svg"
+  },
+  {
+    "revision": "f554f27d42d11cb90968fdbff39fca1b",
+    "url": "./bundleimages/logo-small.svg"
+  },
+  {
+    "revision": "c54be4acb8bfef9a5c2941d838e92f42",
+    "url": "./bundleimages/logo.svg"
+  },
+  {
+    "revision": "9d17cdd532fb79575cc394a6aa698008",
+    "url": "./bundleimages/maestro.svg"
+  },
+  {
+    "revision": "50b1bd2483c8925bd359272d376440a8",
+    "url": "./bundleimages/mastercard.svg"
+  },
+  {
+    "revision": "114a0c361476162e7ab92a80efcf4038",
+    "url": "./bundleimages/no-photo.svg"
+  },
+  {
+    "revision": "794c5f9fc0af2947d89b369df1cf96b2",
+    "url": "./bundleimages/quote-icon.svg"
+  },
+  {
+    "revision": "15cc91d8fa045c71c7e65c5d72917410",
+    "url": "./bundleimages/search.svg"
+  },
+  {
+    "revision": "2cbed1f3f96e36f656d71fd4d20b4322",
+    "url": "./bundleimages/subcategories.svg"
+  },
+  {
+    "revision": "9dcdf877c40d0fe282982ed72e8a03a8",
+    "url": "./bundleimages/twitter-icon.svg"
+  },
+  {
+    "revision": "5a93a28a584dc3157f803e6176b7181d",
+    "url": "./bundleimages/user.svg"
+  },
+  {
+    "revision": "eb63482e3c6153359a36eb5abc604bde",
+    "url": "./bundleimages/visa.svg"
+  },
+  {
+    "revision": "ebc70e6743b684542437cef59a35c39f",
+    "url": "./bundleimages/x.svg"
+  },
+  {
+    "revision": "26d3b536ab5f7bb27cef97c951b7c726",
+    "url": "./bundleimages/youtube-icon.svg"
+  },
+  {
+    "revision": "5a2674ac01fab4c79864",
+    "url": "./bundlejs/app.fd3e9e1cabae0ce4c0b8.js"
+  }
+]);
